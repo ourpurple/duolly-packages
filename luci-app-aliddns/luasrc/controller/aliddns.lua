6a3c1469677985707f18abf5c12cd7a366a8cbd1
ourpurple <ourpurple@qq.com>
2021-04-30 11:13:48 +0800

    Update aliddns.lua

--- luci-app-aliddns/luasrc/controller/aliddns.lua
+++ luci-app-aliddns/luasrc/controller/aliddns.lua
@@ -1,4 +1,4 @@
 module("luci.controller.aliddns",package.seeall)
 function index()
-entry({"admin","network","aliddns"},cbi("aliddns"),_("AliDDNS"),58).acl_depends = { "luci-app-aliddns" }
+entry({"admin","services","aliddns"},cbi("aliddns"),_("AliDDNS"),58).acl_depends = { "luci-app-aliddns" }
 end
